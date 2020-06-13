96ee523ab2b5cd24bf2a285541b85859ec1fd059
Khalid A <Khalid@Baders-MacBook-Air.local>
2020-06-13 14:45:12 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/Authors')
+
+
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb' , extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -27,5 +32,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     
 
 app.use('/' , indexRouter)
+app.use('/Authors' , authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
